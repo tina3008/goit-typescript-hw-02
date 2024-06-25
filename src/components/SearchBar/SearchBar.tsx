47860b3cbc7c13388060fdbe93f06a94283e7944
tina3008 <kicja_3@ukr.net>
2024-06-25 21:04:20 +0300

    target

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -2,17 +2,22 @@ import toast, { Toaster } from "react-hot-toast";
 
 import css from "./SearchBar.module.css";
 
+interface Target extends EventTarget {
+  query: HTMLInputElement;
+}
+
 interface SearchBarProps {
-  onSearch: (searchImg: string) => void;
+  onSearch: (query: string) => void;
 }
 
 export default function SearchBar({ onSearch }: SearchBarProps) {
+
   const handleSubmit = (evt: React.FormEvent<HTMLFormElement>) => {
     evt.preventDefault();
-    const form = evt.target as HTMLFormElement;
-    const searchImg =
-      form.elements.searchImg.value;
-    if (searchImg.trim() === "") {
+    const form = evt.target as Target;
+    const query = form.query.value;
+
+    if (query.trim() === "") {
       toast("Please fill in search folder", {
         style: {
           color: "red",
@@ -21,7 +26,7 @@ export default function SearchBar({ onSearch }: SearchBarProps) {
       return;
     }
 
-    onSearch(searchImg);
+    onSearch(query);   
     form.reset();
   };
 
@@ -31,7 +36,7 @@ export default function SearchBar({ onSearch }: SearchBarProps) {
         <input
           className={css.input}
           type="text"
-          name="searchImg"
+          name="query"
           autoComplete="off"
           autoFocus
           placeholder="Search images and photos"
